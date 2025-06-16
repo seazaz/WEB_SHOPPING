a721cecb8a0d7c62fbd69ca6a5f50474d0abdafa
seazaz <promsakorn5@gmail.com>
2025-06-17 00:40:45 +0700

    newnavbar

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function toggle_cart(){
     let cart_popup = document.getElementById("stuff");
     cart_popup.classList.toggle("show")
 }
+
+function toggle_search(){
+    let search = document.getElementById("search-box")
+    let search_hide = document.getElementById("search")
+    let submit = document.getElementById("submit")
+    search.classList.toggle("show_search")
+    search_hide.classList.toggle("show_search")
+    submit.classList.toggle("show_search")
+}
